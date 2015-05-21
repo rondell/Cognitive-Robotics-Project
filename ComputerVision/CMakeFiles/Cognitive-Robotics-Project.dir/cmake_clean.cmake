78bec40e6abea55c8ea80d624efaf7e5c814ec6f
Emanuele De Donatis <emanuele.dedonatis@gmail.com>
2015-05-21 13:33:01 +0200

    OpenCV branch init

---
+++ ComputerVision/CMakeFiles/Cognitive-Robotics-Project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cognitive-Robotics-Project.dir/main.cpp.o"
+  "Cognitive-Robotics-Project.pdb"
+  "Cognitive-Robotics-Project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cognitive-Robotics-Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ComputerVision/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+/*
+ * 
+ */
+int main() {
+    cout << "OpenCV version: " << CV_VERSION << endl;
+}
+
