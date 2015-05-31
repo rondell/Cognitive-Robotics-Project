852956d984cde7ea5642ed498763552fbd51d47c
rondell <diego.rondelli@mail.polimi.it>
2015-05-31 10:02:53 +0100

    init catkin workspace and add prosilica drivers
    Signed-off-by: rondell <diego.rondelli@mail.polimi.it>

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/osboxes/Desktop/Cognitive-Robotics-Project/catkin_ws/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/osboxes/Desktop/Cognitive-Robotics-Project/catkin_ws/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/osboxes/Desktop/Cognitive-Robotics-Project/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/osboxes/Desktop/Cognitive-Robotics-Project/catkin_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/osboxes/Desktop/Cognitive-Robotics-Project/catkin_ws/devel/lib:/home/osboxes/Desktop/Cognitive-Robotics-Project/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/osboxes/Desktop/Cognitive-Robotics-Project/catkin_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/osboxes/Desktop/Cognitive-Robotics-Project/catkin_ws/devel/lib/pkgconfig:/home/osboxes/Desktop/Cognitive-Robotics-Project/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/osboxes/Desktop/Cognitive-Robotics-Project/catkin_ws/build"
+export PYTHONPATH="/home/osboxes/Desktop/Cognitive-Robotics-Project/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/osboxes/Desktop/Cognitive-Robotics-Project/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/osboxes/Desktop/Cognitive-Robotics-Project/catkin_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
